3795156b109a18ab865a23a5355840767eb08de1
ISalihKilic <isalihkilic@gmail.com>
2020-10-13 09:52:30 +0300

    Update heroku_funcs.sh

--- lib/heroku_funcs.sh
+++ lib/heroku_funcs.sh
@@ -3,8 +3,7 @@ function download_nakama_binary() {
 
   if [ ! -f $CACHE_DIR/nakama ]; then
     output_section "Downloading Nakama Binary..."
-    curl -L $NAKAMA_BINARY_URL | tar -xvf - -C /
-    unzip nakama.zip
+    curl -L $NAKAMA_BINARY_URL | tar -xvz - -C /
     cp nakama-2.12.0-linux-amd64 $CACHE_DIR/nakama
     # cp -i nakama /usr/local/bin
     touch "$CACHE_DIR/._sc_"
